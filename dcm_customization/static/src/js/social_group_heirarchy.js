8cd700ef1f4cba1e87828dec8493998d5336d555
hardik <kalariya.hardik2010@gmail.com>
2021-06-06 11:06:25 +0530

    [IMP] Heirarchical structure

--- dcm_customization/static/src/js/social_group_heirarchy.js
+++ dcm_customization/static/src/js/social_group_heirarchy.js
@@ -4,6 +4,31 @@ odoo.define('dcm_customization.social_group_heirarchy', function (require) {
     var ajax = require('web.ajax');
     
     $(document).ready(function(){
+    	if($('.social_group_hierarchy').length > 0) {
+    		ajax.jsonRpc('/get_tree_heirarchy_details','call',{'search' : $('input[name="social_group_search"]').val(),'social_group_id' : $('input[name="social_group_id"]').val()}).then(function(result){
+    			var count = 0
+    			_.each(result,function(value,key){
+    				console.log("Key==",key,value)
+    				
+    				var temp_div = '<div id="tree_' + count.toString() + '"></div><br/>';
+    				
+    				$('.social_group_hierarchy').append(temp_div)
+    				
+    				$('#tree_' + count.toString()).tree({
+    					data: [value],
+    					autoOpen: false,
+    					selectable: false,
+    					autoEscape : false,
+    					onCreateLi: function(node, $li) {
+    						$li.find('.jqtree-toggler').attr('aria-hidden',false);
+    					},
+    				})
+    				count += 1
+    			})
+    		})
+    	}
+    	
+    	
     	if($('#chart-container').length > 0) {
     		ajax.jsonRpc('/get_heirarchy_details','call',{'social_group_id' : $('#chart-container').attr('group-id')}).then(function(result){
     			console.log("Result---",result)
